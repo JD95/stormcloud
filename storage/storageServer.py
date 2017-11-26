df91614a66c37a1496b247f9ff3daa1af4d4377f
Cameron <C_franke1@u.pacific.edu>
2017-11-26 14:54:40 -0800

    storage server infrastructure

--- storage/storageServer.py
+++ storage/storageServer.py
@@ -4,78 +4,92 @@ import base64
 import nacl
 import nacl.secret
 
+class storageServer: 
 
-with open("notKeys.bin", "rb") as b:
-	f = b.read()
-	key = f
-	box = nacl.secret.SecretBox(key)
-	#f = str(base64.b16encode(f))
-	#f = f.replace("b'","").replace("'","")
-	#print(f)
-
-	m = box.encrypt(b"message")
-	print(repr(m))
-	print(box.decrypt(m))
-
-def listen():
-	connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-	connection.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-	connection.bind(('0.0.0.0', 5555))
-	connection.listen(10)
-	while True:
-		current_connection, address = connection.accept()
-		print(address)
-		while True:
-			data = current_connection.recv(2048)
-			print("got a message")
-			if "random" in data:
-				#print("'You' is trying to connect, do something.. or not, whatever.")
-				#print(data.split("random")[0])
-				message = b"SCHWIFTY"
-				encrypted = box.encrypt(message)
-				current_connection.send(encrypted)
-
-			if True:
-				print("message: " + repr(data) + "\nend of message")
-				data = data.split("\r\n")[1]
-				print("parsed", data)
-				data = base64.b16decode(data) 
-				print(repr(data))
-				#message = data.split("hello")
-				#message.replace("\r\n\r\n","")
-				text = box.decrypt(data)
-				print(text)
-				
-				message =  b"SCHWIFTY"
-				encrypted = box.encrypt(message)
-				print(encrypted)
-				encrypted = base64.b16encode(encrypted)
-				current_connection.send(b"test\r\n" + encrypted + b"\r\n\r\n")
-				print("message sent")
-				break
+	def __init__(self):
+		#read in secret key from file 
+		with open("notKeys.bin", "rb") as b:
+			f = b.read()
+			key = f
+			self.box = nacl.secret.SecretBox(key)
+		
+			self.connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        	self.connection.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        	self.connection.bind(('0.0.0.0', 5555))
 			
+			self.logString = ""
 
-			#packet format: text descriptor, \r\n, payload, \r\n\r\n
-			'''
-			if data == 'quit\r\n':
-				current_connection.shutdown(1)
-				current_connection.close()
-				break
 
-			elif data == 'stop\r\n':
-				current_connection.shutdown(1)
-				current_connection.close()
-				exit()
+	def listen(self):
+		self.connection.listen(10)#what does the 10 mean here? 
+		while True:
+			current_connection, address = self.connection.accept()
+			print("MAKE SURE TO VERIFY THAT THE MESAGE IS FROM THE EXPECTED IP"+ str(address))
+			while True:
+				data = current_connection.recv(2048)
+				print("got a message")
+	
+				if "store" in data:
+					print("Stub: implement store")
+					self.store(data)
 
-			elif data:
-				print("sending data back")
-				current_connection.send(data)
-				print("data: " +repr(data))
-				print("done sending")
-			'''
+				if "retrieve" in data: 
+					print("Stub: implement retrieve")
+					self.retrieve(data)
 
-if __name__ == "__main__":
-	try:
-		listen()
-	except KeyboardInterrupt:
-		pass
+				if "delete" in data:
+					print("Stub: implement delete")
+					self.delete(data)
+
+
+				################################## sample send/ recieve ###############
+				if "random" in data:
+					message = b"SCHWIFTY"
+					encrypted = box.encrypt(message)
+					current_connection.send(encrypted)
+
+
+				if True:
+					print("raw message: " + repr(data) + "\nend of message")
+					data = data.split("\r\n")[1]
+					data = base64.b16decode(data) 
+					print(repr(data))
+					text = box.decrypt(data)
+					print("plaintext: " + str(text))
+				
+					message =  b"SCHWIFTY"
+					encrypted = box.encrypt(message)
+					#print(encrypted)
+					encrypted = base64.b16encode(encrypted)
+					current_connection.send(b"test\r\n" + encrypted + b"\r\n\r\n")
+					print("encrypted message sent")
+					break
+				####################################
+				
+	def store(self, packet): 
+		## format: 'store\r\n' <file name hash> '\r\n' <encrypted file data> '\r\n\r\n'
+		self.logString += "s"
+
+	def retrieve(self, packet): 
+		## format: 'retrieve\r\n' <file name hash> '\r\n\r\n'
+		self.logString += "r"
+
+	def delete(self, packet): 
+		## delete: 'delete\r\n' <file name hash> '\r\n\r\n'
+		self.logString += "d"
+
+	def respond(self, commandType):
+		response = str(commandType) + "\r\n"
+		if commandType == "retrieve":
+			print("append return payload")
+		response += self.logString[-50:] + "\r\n\r\n"
+		print("return message: " + str(response))
+		print("NEED TO IMPLEMENT SENDING OF RESPONSE STRING")
+
+		
+		
+
+
+ 
+x = storageServer()
+x.listen()
